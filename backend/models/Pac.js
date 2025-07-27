0dbc3460ce9926e8239eaf170204d0da09675ef2
Tony Lee <tonyklee@umich.edu>
2025-07-27 06:52:28 -0400

    fix:cleaning up

--- backend/models/Pac.js
+++
@@ -1,9 +0,0 @@
-// models/Pec.js --Eg
-const mongoose = require('mongoose');
-const PacSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  language: String,
-  tag: String,
-  user: String, // Add whatever fields are required by business logic
-});
-module.exports = mongoose.model('Pac', PacSchema);
--- backend/models/Tec.js
+++
@@ -1,9 +0,0 @@
-// models/Tec.js --Eg
-const mongoose = require('mongoose');
-const TecSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  language: String,
-  tag: String,
-  user: String, // Add whatever fields are required by business logic
-});
-module.exports = mongoose.model('Tec', TecSchema);
